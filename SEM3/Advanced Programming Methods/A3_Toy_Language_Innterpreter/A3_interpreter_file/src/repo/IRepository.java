3703cc617408b2db45498473d73dba56cb78a84a
913-Goian-Sergiu <111054029+Sergiu2404@users.noreply.github.com>
2024-02-08 22:43:57 +0200

    Add files via upload

---
+++ SEM3/Advanced Programming Methods/A3_Toy_Language_Innterpreter/A3_interpreter_file/src/repo/IRepository.java
@@ -0,0 +1,14 @@
+package repo;
+
+import model.programState.ProgramState;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public interface IRepository {
+    void addProgram(ProgramState newProgram);
+
+    ProgramState getCurrentProgram();
+
+    void logPrgStateExec() throws IOException;
+}
