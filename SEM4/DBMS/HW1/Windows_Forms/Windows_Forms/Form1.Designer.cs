b57210ac33a69a052f52fbba2ff250e4de5fb405
913-Goian-Sergiu <111054029+Sergiu2404@users.noreply.github.com>
2024-07-30 15:12:35 +0300

    AI and DBMS

---
+++ SEM4/DBMS/HW1/Windows_Forms/Windows_Forms/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Windows_Forms
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sportsDataGridView = new System.Windows.Forms.DataGridView();
+            this.teamsDataGridView = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.sportsDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.teamsDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sportsDataGridView
+            // 
+            this.sportsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.sportsDataGridView.Location = new System.Drawing.Point(833, 12);
+            this.sportsDataGridView.Name = "sportsDataGridView";
+            this.sportsDataGridView.RowHeadersWidth = 51;
+            this.sportsDataGridView.RowTemplate.Height = 24;
+            this.sportsDataGridView.Size = new System.Drawing.Size(438, 286);
+            this.sportsDataGridView.TabIndex = 0;
+            // 
+            // teamsDataGridView
+            // 
+            this.teamsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.teamsDataGridView.Location = new System.Drawing.Point(302, 12);
+            this.teamsDataGridView.Name = "teamsDataGridView";
+            this.teamsDataGridView.RowHeadersWidth = 51;
+            this.teamsDataGridView.RowTemplate.Height = 24;
+            this.teamsDataGridView.Size = new System.Drawing.Size(478, 286);
+            this.teamsDataGridView.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.AccessibleDescription = "updateButton";
+            this.button1.AccessibleName = "updateButton";
+            this.button1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.button1.Location = new System.Drawing.Point(759, 368);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 50);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "update";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.updateEvent);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(527, 329);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Teams";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1035, 329);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Sports";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1717, 716);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.teamsDataGridView);
+            this.Controls.Add(this.sportsDataGridView);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.sportsDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.teamsDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView sportsDataGridView;
+        private System.Windows.Forms.DataGridView teamsDataGridView;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ SEM4/DBMS/HW1/Windows_Forms/Windows_Forms/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//needed for Sql conn
+using System.Data.SqlClient;
+
+namespace Windows_Forms
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connection;
+        SqlDataAdapter adapterSports;
+        SqlDataAdapter adapterTeams;
+
+        DataSet dataset;
+
+        BindingSource bindingSports;
+        BindingSource bindingTeams;
+
+        SqlCommandBuilder commandBuilder;
+
+        string querySports;
+        string queryTeams;
+
+        public Form1()
+        {
+            InitializeComponent();
+            FillData();
+        }
+
+
+        string getConnectionString()
+        {
+            return "Data Source=DESKTOP-EBHU28B\\SQLEXPRESS;" +
+                "Initial Catalog=Flashscore;Integrated Security=true;";
+
+        }
+
+
+        void FillData()
+        {
+            connection = new SqlConnection(getConnectionString());
+
+            querySports = "select * from SportsTable";
+            queryTeams = "select * from TeamsTable";
+
+            adapterSports = new SqlDataAdapter(querySports, connection);
+            adapterTeams = new SqlDataAdapter(queryTeams, connection);
+
+            dataset = new DataSet();
+
+            adapterSports.Fill(dataset, "SportsTable");
+            adapterTeams.Fill(dataset, "TeamsTable");
+
+            //fill in insert, update, and delete commands
+            commandBuilder = new SqlCommandBuilder(adapterTeams);
+
+            //DataRelation (parent-child rel) added to the dataset
+            dataset.Relations.Add(
+                "SportsTeams",
+                dataset.Tables["SportsTable"].Columns["sportId"],
+                dataset.Tables["TeamsTable"].Columns["sportId"]
+                );
+
+
+            //METH 1: fill data into DataGridViews using properties DataSource, DataMember
+            this.sportsDataGridView.DataSource = dataset.Tables["SportsTable"];
+            this.teamsDataGridView.DataSource = this.sportsDataGridView.DataSource;
+            this.teamsDataGridView.DataMember = "SportsTeams";
+
+            //METH 2: fill data into DataGridViews using Data Binding
+            //bindingSports = new BindingSource();
+            //bindingSports.DataSource = dataset.Tables["Sports"];
+            //bindingTeams = new BindingSource(bindingSports, "SportsTeams");
+
+            //this.dataGridView1.DataSource = bindingSports;
+            //this.dataGridView2.DataSource = bindingTeams;
+
+            commandBuilder.GetUpdateCommand();
+        }
+
+
+        private void updateEvent(object sender, EventArgs e)
+        {
+            try
+            {
+                adapterTeams.Update(dataset, "TeamsTable");
+            }catch(Exception exception)
+            {
+                MessageBox.Show("Database update did not work\n" + exception.Message, "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Console.WriteLine(exception.ToString());
+            }
+        }
+
+    }
+}
